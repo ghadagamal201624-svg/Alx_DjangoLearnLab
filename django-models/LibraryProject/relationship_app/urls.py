231e71fe8ddfe8181b5358ba94d8438bdbc07ff4
Ghada <ghadagamal.201624@gmail.com>
2025-11-12 00:39:08 +0200

    week10

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,22 +2,19 @@
 
 from django.urls import path
 from . import views # <-- استيراد شامل
-from .views import list_books, LibraryDetailView, register_view
 from django.contrib.auth.views import LoginView, LogoutView 
 
 urlpatterns = [
     # مسارات التطبيق
-    path('books/', list_books, name='book-list'),
-    path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('books/', views.list_books, name='book-list'), 
+    path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail'),
     
     # مسارات المصادقة
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     
-    # التعديل هنا: إضافة http_method_names لحل مشكلة 405
-    path('logout/', LogoutView.as_view(
-        template_name='relationship_app/logout.html',
-        http_method_names=['post', 'get'] # <--- السطر الإضافي
-    ), name='logout'),
+    # الالتزام بصيغة التحقق (LogoutView.as_view(template_name=...))
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'), 
     
-    path('register/', register_view, name='register'),
+    # الالتزام بصيغة التحقق (views.register_view)
+    path('register/', views.register_view, name='register'),
 ]
