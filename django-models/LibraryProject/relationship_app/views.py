b1d5fbbcf27b058920eae98e76aa08ff4ec82cbd
Ghada <ghadagamal.201624@gmail.com>
2025-11-11 18:49:09 +0200

    week 10

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,11 @@
 # relationship_app/views.py
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library, Author
 
-# 1. Function-based View (FBV)
+# 1. Function-based View (FBV) for books 
 def book_list_view(request):
-    all_books = Book.objects.select_related('author').all()
+    all_books = Book.objects.all()
     context = {'books': all_books}
     return render(request, 'relationship_app/list_books.html', context)
 
@@ -14,4 +14,3 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-    
