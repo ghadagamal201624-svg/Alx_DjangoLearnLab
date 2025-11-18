580b0aeba66442be0f5134e67514c919f81c2210
Ghada <ghadagamal.201624@gmail.com>
2025-11-19 00:04:11 +0200

    week11

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,19 +1,27 @@
-from django.shortcuts import render
-from .forms import Exampleform 
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm # تم تصحيح حالة الأحرف (F)
+from .models import Book # يجب استيراد Book و النماذج الأخرى
 
+# ------------------------------------
+# الـ Views المؤمّنة بالأذونات المخصصة
+# ------------------------------------
 
-#4. View لعرض كتاب (يتطلب إذن 'can_view')
+# 4. View لعرض كتاب (يتطلب إذن 'can_view')
 @permission_required('relationship_app.can_view', login_url='/login/')
-def secured_book_list_viwe(request):
+def secured_book_list_view(request): # تم تصحيح viwe -> view
     all_books = Book.objects.all()
-    return render(request, 'relashionship_app/list_book.html', {'books': all:books})
+    # تم تصحيح اسم القالب والسياق
+    return render(request, 'relationship_app/list_books.html', {'books': all_books}) 
 
 # 5. View لإنشاء كتاب (يتطلب إذن 'can_create')
-@permission_required('relationship_app.can_create', raise_exception=true)
-def book_create_viwe(request):
+@permission_required('relationship_app.can_create', raise_exception=True) # تم تصحيح true -> True
+def book_create_view(request): # تم تصحيح viwe -> view
     return render(request, 'relationship_app/permission_message.html', {'action': 'Create Book'})
 
 # 6. View لتعديل كتاب (يتطلب إذن 'can_edit')
+# يجب إضافة الديكوراتور إذا كانت مؤمنة
+@permission_required('relationship_app.can_edit', raise_exception=True) 
 def book_edit_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'relationship_app/permission_message.html', {'action': f'Edit {book.title}'})
@@ -23,4 +31,3 @@ def book_edit_view(request, pk):
 def book_delete_view(request, pk):
     book = get_object_or_404(Book, pk=pk)
     return render(request, 'relationship_app/permission_message.html', {'action': f'Delete {book.title}'})
-
