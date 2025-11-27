db38db832b40fdab8fa3421d9a2d3c0eaa496f43
Ghada <ghadagamal.201624@gmail.com>
2025-11-27 02:59:48 +0200

    week13

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,35 +1,20 @@
-from rest_framework import serializers 
+from rest_framework import serializers
 from .models import Author, Book
-from datetime import date 
+from datetime import date
 
-class Bookserializer(serializers.ModelSerializer):
-    """
-    Serializer for the Book model.
-    Handles serialization of book fields and custom validation for publication_year.
-    """
-    class meta:
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Book
-        firlds = '__all__'
+        fields = '__all__'
 
     def validate_publication_year(self, value):
-        """
-        Check that the publication year is not in the future.
-        """
         current_year = date.today().year
         if value > current_year:
-            raise serializers.ValidationError("publication year cannot be in the future.")
+            raise serializers.ValidationError("Publication year cannot be in the future.")
         return value
-    
-    
-class Authorserializer(serializers.ModelSerializer):
-    """
-    serializer for the Author model.
-    Includes a nested BookSerializer to display related books dynamically.
-    """
-    # Nested serializer:
-    # many=True: لأن المؤلف قد يكون له أكثر من كتاب
-    # read_only=True: لأننا نستخدمه للعرض حالياً، ولتجنب تعقيد الإنشاء في هذه المرحلة
-    books = Bookserializer(many=True, read_only=True)
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
 
     class Meta:
         model = Author
