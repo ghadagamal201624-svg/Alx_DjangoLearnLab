318723bd884a6ffa19ccc5acf267ace136ea09d2
Ghada <ghadagamal.201624@gmail.com>
2025-12-13 19:21:42 +0200

    week 15

--- social_media_api/social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/social_media_api/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 AUTH_USER_MODEL = 'accounts.User'
 
@@ -129,3 +129,6 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+
+STATIC_URL = 'static/'
