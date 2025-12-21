f6b3b8d9db83824b4eea1332b20c200f47872e9e
Ghada <ghadagamal.201624@gmail.com>
2025-12-22 00:01:36 +0200

    Implement posts and comments apps with CRUD and permissions

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,28 @@
+from rest_framework import viewsets, permissions, filters
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+# صلاحية مخصصة: تسمح فقط لصاحب المحتوى بتعديله أو حذفه
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content'] # تفعيل البحث بالعنوان أو المحتوى
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'accounts',
+    'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    # ... المصادقة التي أضفناها سابقاً
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10, # عدد المنشورات في الصفحة الواحدة
+}
