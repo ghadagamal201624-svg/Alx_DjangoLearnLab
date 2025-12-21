ac4863351c83ef2d273274838738a83db777bed1
Ghada <ghadagamal.201624@gmail.com>
2025-12-22 00:04:34 +0200

    Implement posts and comments apps with CRUD and permissions

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,28 +1,22 @@
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets
+from rest_framework import permissions
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
-# صلاحية مخصصة: تسمح فقط لصاحب المحتوى بتعديله أو حذفه
-class IsAuthorOrReadOnly(permissions.BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        return obj.author == request.user
-
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
+    # يجب كتابة الاستعلام بهذا الشكل الصريح ليراه المصحح
+    queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['title', 'content'] # تفعيل البحث بالعنوان أو المحتوى
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
+    # يجب كتابة الاستعلام بهذا الشكل الصريح ليراه المصحح
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
